ed5570dc3d67ea08d103282d91c953c0c2b182f6
Marius Barbulescu <marius.barbulescu@gmail.com>
2022-12-10 15:41:58 +0100

    day 8 - second part

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -15,8 +15,8 @@ fun main() {
         .markFromTop()
         .markFromBottom()
 
-    println(grid)
     println("Visible: ${grid.countVisible()}")
+    grid.calculateHighestScenicScore()
 }
 
 private class Grid(values: List<List<Int>>) {
@@ -81,6 +81,44 @@ private class Grid(values: List<List<Int>>) {
         return storage
             .flatten()
             .count { it.visible }
+    }
+
+    fun calculateHighestScenicScore() {
+        var highest = 0
+        for (row in (1..size - 2)) {
+            for (col in (1..size - 2)) {
+                val score = calculateScenicScore(row, col)
+                if (score > highest) {
+                    highest = score
+                }
+            }
+        }
+        println("Highest score is $highest")
+    }
+
+    private fun calculateScenicScore(row: Int, col: Int): Int {
+        val tree = storage[row][col].value
+        val score1 = calculateScore(tree, (row - 1 downTo 0)) { storage[it][col] }
+        val score2 = calculateScore(tree, (row + 1 until size)) { storage[it][col] }
+        val score3 = calculateScore(tree, (col - 1 downTo 0)) { storage[row][it] }
+        val score4 = calculateScore(tree, (col + 1 until size)) { storage[row][it] }
+        val score = score1 * score2 * score3 * score4
+        println("Score is $score for position ($row, $col) with $score1 $score2 $score3 $score4")
+        return score
+    }
+
+    private fun calculateScore(tree: Int, range: IntProgression, valueExtractor: (Int) -> Position): Int {
+        val values = range
+            .map(valueExtractor)
+            .map { it.value }
+
+        values.forEachIndexed {index, value ->
+            if (value >= tree) {
+                return index + 1
+            }
+        }
+
+        return values.size
     }
 }
 
